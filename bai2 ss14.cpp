21cc6525acfd757cb949db3d2a7741aa31604d69
davidpham26 <phonghaha20065432@gmail.com>
2024-12-12 15:48:22 +0700

    Update bai2 ss14.cpp

--- bai2 ss14.cpp
+++ bai2 ss14.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char chuoi[] = "Xin chao, the gioi!";
+    char chuoi[] = "Xin chao, minh la sinh vien PTIT!";
     int i = 0;
 
     while (chuoi[i] != '\0') {
